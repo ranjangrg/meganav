818f6c96f27eeea892a154afaab2ad75bee4204a
ranjangrg <32595621+ranjangrg@users.noreply.github.com>
2023-12-26 12:57:07 +0000

    Update navigation-module.js

--- navigation-module.js
+++ navigation-module.js
@@ -1,7 +1,11 @@
+/**
+Example file for using navigation controller as a module using imports
+*/
+
 import NavigationController from "./navigation.js";
 // the "NavigationController" class needs to be exported in the source file.
 
 document.addEventListener("DOMContentLoaded", function () {
-    // testing new controller class
+    // creating the new controller class
     let navController = new NavigationController("custom-header");
 });
